dd371610ee81c2cfa11e0f685575d894a8e5026b
Alexander Gorbunov <s-kozelsk@yandex.ru>
2020-04-24 11:17:21 +0300

    Algo-adding panel
    It's working fine now, I need just to work with DB now!

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template, url_for, request, session, redirect
 from cfApi import userInfo
 from random import randint
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 
+interfaces = [
+	'name',
+	'other',
+	'description',
+	'urls',
+	'paste',
+	'user'
+]
 app = Flask(__name__)
 app.config['SECRET_KEY'] = generate_password_hash('Alfred Hitchcock')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/gornak40/code/yandex/algolimp/data/algo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Algo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String, unique=True, nullable=False)
+	other = db.Column(db.String, nullable=False)
+	description = db.Column(db.Text, nullable=False)
+	urls = db.Column(db.Text, nullable=False)
+	paste = db.Column(db.String)
+	user = db.Column(db.String, nullable=False)
 
 
 @app.route('/')
@@ -16,11 +38,33 @@ def index():
 	return render_template('index.html', **kwargs, **session)
 
 
-@app.route('/algo/adding/')
+@app.route('/algo/adding/', methods=['post', 'get'])
 def adding():
 	kwargs = dict()
 	kwargs['title'] = 'Adding'
 	kwargs['content_title'] = 'Adding'
+	if request.method == 'POST':
+		session['algo'] = session.get('algo', dict())
+		name = request.form.get('name')
+		other = request.form.get('other')
+		description = request.form.get('description')
+		urls = request.form.get('urls')
+		paste = request.form.get('paste')
+		user = session.get('user')
+		if name:
+			session['algo']['name'] = name
+		if other:
+			session['algo']['other'] = other
+		if description:
+			session['algo']['description'] = description
+		if urls:
+			session['algo']['urls'] = urls
+		if paste:
+			session['algo']['paste'] = paste
+		session['algo']['user'] = user['handle']
+		if (all([session['algo'].get(i) for i in interfaces])):
+			# здесь я типо базу данных обновляю
+			return redirect('/algo')
 	return render_template('adding.html', **kwargs, **session)
 
 
@@ -44,8 +88,8 @@ def login():
 		code = request.form.get('code')
 		if handle:
 			session['handle'] = handle
-			info = userInfo(handle)
-			session['email'] = info.get('email') if info else None
+			session['user'] = userInfo(handle)
+			session['email'] = session['user'].get('email') if session['user'] else None
 			session['secret'] = str(randint(1000, 9999))
 			session['auth'] = False
 		session['code'] = code if code else str()
@@ -60,11 +104,12 @@ def logout():
 	kwargs = dict()
 	kwargs['title'] = 'Logout'
 	kwargs['content_title'] = 'Logout'
-	session.pop('email') if session.get('email') else None
-	session.pop('code') if session.get('code') else None
-	session.pop('handle') if session.get('handle') else None
-	session.pop('secret') if session.get('secret') else None
-	session.pop('auth') if session.get('auth') else None
+	session.clear()
+	# session.pop('email') if session.get('email') else None
+	# session.pop('code') if session.get('code') else None
+	# session.pop('handle') if session.get('handle') else None
+	# session.pop('secret') if session.get('secret') else None
+	# session.pop('auth') if session.get('auth') else None
 	return render_template('logout.html', **kwargs, **session)
 
 
